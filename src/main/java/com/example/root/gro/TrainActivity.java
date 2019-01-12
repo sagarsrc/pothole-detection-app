66ef3cc929f7a0384e665446d75ce27cee8479fe
Sagar Sarkale <sagxam@gmail.com>
2019-01-12 18:31:23 +0530

    Merge issue resolved

--- src/main/java/com/example/root/gro/TrainActivity.java
+++ src/main/java/com/example/root/gro/TrainActivity.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-package com.example.root.gro;
-
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-public class TrainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_train);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Sensors Started", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-}
-||||||| merged common ancestors
-=======
 package com.example.root.gro;
 
 import android.hardware.Sensor;
@@ -176,4 +144,4 @@ public class TrainActivity extends AppCompatActivity implements SensorEventListe
 
     }
 }
->>>>>>> dev
+
